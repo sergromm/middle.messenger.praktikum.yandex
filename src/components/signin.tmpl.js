8ebf74e4d4538e323b17ae9ca3d3c967da51bf1b
sergromm <sergichev@gmail.com>
2022-02-11 13:05:44 +0300

    add form styles

--- src/components/signin.tmpl.js
+++ src/components/signin.tmpl.js
@@ -1,9 +1,15 @@
 export const signin = `
-  <h1>Войти</h1>
-  <form>
-    <input />
-    <input />
-    <button>Войти</button>
-    <p>Нет аккаунта?<a href="/signup">Регистрация</a></p>
+  
+  <form class="form signin-form">
+    <fieldset class="form-container">
+      <legend class="form-title">Войти</legend>
+      <input placeholder="Логин" type="text" class="input sigin-login"/>
+      <input placeholder="Пароль" type="password" class="input sigin-password"/>
+      <button class="form-button">Войти</button>
+      <p class="form-text">
+        Нет аккаунта?
+        <a class="form-link" href="/signup">Регистрация</a>
+      </p>
+    </fieldset>
   </form>
 `;
--- src/components/signup.tmpl.js
+++ src/components/signup.tmpl.js
@@ -1,12 +1,17 @@
 export const signup = `
-<h1>Регистрация</h1>
-<form>
-  <input />
-  <input />
-  <input />
-  <input />
-  <input />
-  <button>Зарегистрироваться</button>
-  <p>Уже зарегистрированны?<a href="/signin">Войти</a></p>
-</form>
+  <form class="form signup-form">
+    <fieldset class="form-container">
+      <legend class="form-title">Регистрация</legend>
+      <input class="input"/>
+      <input class="input"/>
+      <input class="input"/>
+      <input class="input"/>
+      <input class="input"/>
+      <button class="form-button">Зарегистрироваться</button>
+      <p class="form-text">
+        Уже зарегистрированны?
+        <a class="form-link" href="/signin">Войти</a>
+      </p>
+    </fieldset>
+  </form>
 `;
